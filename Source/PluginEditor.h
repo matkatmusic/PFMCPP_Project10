8e480d9f248d08ee59d2dd07817f0e7cddbad4a8
Charles Schiermeyer <matkatmusic@gmail.com>
2020-01-11 12:56:58 -0800

    initial commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Pfmcpp_project10AudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    Pfmcpp_project10AudioProcessorEditor (Pfmcpp_project10AudioProcessor&);
+    ~Pfmcpp_project10AudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Pfmcpp_project10AudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Pfmcpp_project10AudioProcessorEditor)
+};
